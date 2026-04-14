88540fe486e0592e6a5b265d7e9f3b80fbd1764f
Francisco Estrada <franciscobackend@gmail.com>
2026-04-14 17:36:58 -0500

    (fix) arreglo de bug overlay

--- parchis-pathfinder/src/overlay/__init__.py
+++ parchis-pathfinder/src/overlay/__init__.py
@@ -1,5 +1,5 @@
 """Overlay Renderer module for Parchís Pathfinding."""
 
-from .renderer import OverlayRenderer
+from .renderer import OverlayRenderer, create_overlay, MockOverlayRenderer
 
-__all__ = ["OverlayRenderer"]
+__all__ = ["OverlayRenderer", "create_overlay", "MockOverlayRenderer"]
